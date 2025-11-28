22493f8d3a00304be7ad766d3d801e9e699dd58e
rohan3632 <252-35-183@diu.edu.bd>
2025-11-28 22:39:12 +0600

    Update t.c

--- t.c
+++ t.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
-
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    int a, pri = 0, sec = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &a);
-            if (i == j) pri += a;
-            if (i + j == n - 1) sec += a;
+    #include <stdio.h>
+     
+    int main() {
+        int n;
+        scanf("%d", &n);
+     
+        int a, primary = 0, secondary = 0;
+     
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                scanf("%d", &a);
+                if (i == j) primary += a;
+                if (i + j == n - 1) secondary += a;
+            }
         }
+     
+        int diff = primary - secondary;
+        if (diff < 0) diff = -diff;
+     
+        printf("%d", diff);
+     
+        return 0;
     }
 
-    int diff = primary - secondary;
-    if (diff < 0) diff = -diff;
-
-    printf("%d", diff);
-
-    return 0;
-}
